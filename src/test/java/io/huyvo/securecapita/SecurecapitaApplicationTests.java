48831b6290d9748afbab0184719f828edbde32a6
Vo Dang Huy <iamdaghuy@gmail.com>
2025-11-05 15:37:40 +0700

    first commit

---
+++ src/test/java/io/huyvo/securecapita/SecurecapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.huyvo.securecapita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
