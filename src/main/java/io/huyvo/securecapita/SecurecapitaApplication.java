38aae712d59bbf99f1e4a3f51858ad28ee764838
Vo Dang Huy <iamdaghuy@gmail.com>
2025-11-19 12:55:26 +0700

    Consider defining a bean of type 'org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder' in your configuration.

--- src/main/java/io/huyvo/securecapita/SecurecapitaApplication.java
+++ src/main/java/io/huyvo/securecapita/SecurecapitaApplication.java
@@ -2,12 +2,21 @@ package io.huyvo.securecapita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class SecurecapitaApplication {
+
+	private static final int STRENGTH = 12;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurecapitaApplication.class, args);
 	}
 
+	@Bean
+	public static BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder(STRENGTH);
+	}
 }
