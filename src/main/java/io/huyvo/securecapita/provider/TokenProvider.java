ce4cd2f4d7f393bba71e766d6d0de13348ab13b7
Vo Dang Huy <iamdaghuy@gmail.com>
2025-11-29 15:24:48 +0700

    Add the java-jwt dependency to support JWT implementation; introduce the TokenProvider class with core functionalities createAccessToken and createRefreshToken, along with related supporting methods for the class

---
+++ src/main/java/io/huyvo/securecapita/provider/TokenProvider.java
@@ -0,0 +1,115 @@
+package io.huyvo.securecapita.provider;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.InvalidClaimException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import io.huyvo.securecapita.model.UserPrincipal;
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.Date;
+import java.util.List;
+
+import static java.lang.System.currentTimeMillis;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+
+@Component
+public class TokenProvider {
+
+    private static final String ISSUER = "VODANGHUY";
+    private static final String CUSTOM_MANAGEMENT_SERVICE = "CUSTOM_MANAGEMENT_SERVICE";
+    private static final String AUTHORITIES = "authorities";
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 1_800_000; // 30 minutes
+    private static final long REFRESH_TOKEN_EXPIRATION_TIME = 432_000_000;
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String createAccessToken(UserPrincipal userPrincipal){
+        return JWT.create()
+                .withIssuer(ISSUER)
+                .withAudience(CUSTOM_MANAGEMENT_SERVICE)
+                .withIssuedAt(new Date())
+                .withSubject(userPrincipal.getUsername())
+                .withArrayClaim(AUTHORITIES, getClaimsFromUser(userPrincipal))
+                .withExpiresAt(new Date(currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(secret.getBytes()));
+    }
+
+    public String createRefreshToken(UserPrincipal userPrincipal){
+        return JWT.create()
+                .withIssuer(ISSUER)
+                .withAudience(CUSTOM_MANAGEMENT_SERVICE)
+                .withSubject(userPrincipal.getUsername())
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(currentTimeMillis() + REFRESH_TOKEN_EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(secret.getBytes()));
+    }
+
+    public String getSubject(String token, HttpServletRequest request){
+        try{
+            JWTVerifier verifier = getJWTVerifier();
+            return verifier.verify(token).getSubject();
+        }catch (TokenExpiredException exception){
+            request.setAttribute("expiredMessage", exception.getMessage());
+            throw exception;
+        }catch (InvalidClaimException exception){
+            request.setAttribute("invalidClaim", exception.getMessage());
+            throw exception;
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    public List<GrantedAuthority> getAuthorities(String token){
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(toList());
+    }
+
+    public Authentication getAuthentication(String email,List<GrantedAuthority> authorities, HttpServletRequest request){
+        UsernamePasswordAuthenticationToken usernamePasswordAuthToken = new UsernamePasswordAuthenticationToken(email, null, authorities);
+        usernamePasswordAuthToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return usernamePasswordAuthToken;
+    }
+
+    public boolean isTokenValid(String email, String token){
+        JWTVerifier verifier = getJWTVerifier();
+        return StringUtils.isNotEmpty(email) && !isTokenExpired(verifier, token);
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try{
+            Algorithm algorithm = Algorithm.HMAC512(secret);
+            verifier = JWT.require(algorithm).withIssuer(ISSUER).build();
+        }catch (JWTVerificationException exception){
+            throw new JWTVerificationException("Token cannot be verified");
+        }
+        return verifier;
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+        return userPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toArray(String[]::new);
+    }
+}
