764f8ffbda51ecf283797889e9a4c32f86170f1f
Vo Dang Huy <iamdaghuy@gmail.com>
2025-12-07 11:16:45 +0700

    Add the "/user/register/**" value to PUBLIC_URLS in the SecurityConfig class.

--- src/main/java/io/huyvo/securecapita/configuration/SecurityConfig.java
+++ src/main/java/io/huyvo/securecapita/configuration/SecurityConfig.java
@@ -34,7 +34,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private static final String[] PUBLIC_URLS = { "/user/login/**", "/user/verify/code/**" };
+    private static final String[] PUBLIC_URLS = { "/user/login/**", "/user/verify/code/**", "/user/register/**" };
     private final BCryptPasswordEncoder encoder;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
